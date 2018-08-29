60475b589d6935051cad98c833229dd14848b833
Max Heiber <max.heiber@gmail.com>
2018-08-28 18:20:17 -0700

    handle private names in generics

---
+++ tests/baselines/reference/privateNamesInGenericClasses.js
@@ -0,0 +1,27 @@
+//// [privateNamesInGenericClasses.ts]
+class C<T> {
+  #foo: T;
+  bar(x: C<T>) { return x.#foo; }          // OK
+  baz(x: C<number>) { return x.#foo; }     // OK
+  quux(x: C<string>) { return x.#foo; }    // OK
+}
+
+declare let a: C<number>;
+declare let b: C<string>;
+a.#foo;                                   // OK
+a = b;                                    // Error
+b = a;                                    // Error
+
+
+//// [privateNamesInGenericClasses.js]
+var C = /** @class */ (function () {
+    function C() {
+    }
+    C.prototype.bar = function (x) { return x.#foo; }; // OK
+    C.prototype.baz = function (x) { return x.#foo; }; // OK
+    C.prototype.quux = function (x) { return x.#foo; }; // OK
+    return C;
+}());
+a.#foo; // OK
+a = b; // Error
+b = a; // Error
