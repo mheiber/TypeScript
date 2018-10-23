609706b8053ee83ad8fe5a6912e867e6d5e8ef17
Joseph Watts <jwatts43@bloomberg.net>
2018-10-23 17:39:22 -0400

    Add test case for WeakMap declaration
    Signed-off-by: Joseph Watts <jwatts43@bloomberg.net>

---
+++ tests/baselines/reference/privateNameFieldDeclaration.js
@@ -0,0 +1,14 @@
+//// [privateNameFieldDeclaration.ts]
+class A {
+    #name: string;
+}
+
+
+//// [privateNameFieldDeclaration.js]
+var _name;
+var A = /** @class */ (function () {
+    function A() {
+    }
+    return A;
+}());
+_name = new WeakMap();
