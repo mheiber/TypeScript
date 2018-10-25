083b4cb8859a8d805a2449db024150d232631b56
Joseph Watts <jwatts43@bloomberg.net>
2018-10-25 12:52:31 -0400

    Accept test change
    Signed-off-by: Joseph Watts <jwatts43@bloomberg.net>

--- tests/baselines/reference/privateNameField.js
+++ tests/baselines/reference/privateNameField.js
@@ -7,9 +7,12 @@ class A {
 }
 
 //// [privateNameField.js]
+var _classPrivateFieldSet = function (receiver, privateMap, value) { if (!privateMap.has(receiver)) { throw new TypeError("attempted to set private field on non-instance"); } privateMap.set(receiver, value); return value; };
+var _name;
 var A = /** @class */ (function () {
     function A(name) {
-        this.#name = name;
+        _classPrivateFieldSet(this, _name, name);
     }
     return A;
 }());
+_name = new WeakMap();
