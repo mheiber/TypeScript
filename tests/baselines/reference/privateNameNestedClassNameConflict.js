4d46da12c6f1372e01293f14732f1a23ae9febfa
Max Heiber <mheiber@bloomberg.net>
2018-12-17 14:05:29 -0500

    Add test for private name conflicts with nesting
    Add test case for the naming conflict that can
    happen with generated variable names when downleveling
    nested classes with private names.
    
    Signed-off-by: Max Heiber <mheiber@bloomberg.net>

---
+++ tests/baselines/reference/privateNameNestedClassNameConflict.js
@@ -0,0 +1,27 @@
+//// [privateNameNestedClassNameConflict.ts]
+class A {
+    #foo: string;
+    constructor () {
+        class A {
+            #foo: string;
+        }
+    }
+}
+
+//// [privateNameNestedClassNameConflict.js]
+var _foo;
+var A = /** @class */ (function () {
+    function A() {
+        _foo.set(this, void 0);
+        var _foo;
+        var A = /** @class */ (function () {
+            function A() {
+                _foo.set(this, void 0);
+            }
+            return A;
+        }());
+        _foo = new WeakMap();
+    }
+    return A;
+}());
+_foo = new WeakMap();
