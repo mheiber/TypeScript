a2e31fcd275f89338f576af9e43f38c197d11edf
Max Heiber <max.heiber@gmail.com>
2019-07-03 16:19:14 +0100

    intern private name descriptions

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -599,6 +599,7 @@ namespace ts {
         let sourceText: string;
         let nodeCount: number;
         let identifiers: Map<string>;
+        let privateNames: Map<string>;
         let identifierCount: number;
 
         let parsingContext: ParsingContext;
@@ -789,6 +790,7 @@ namespace ts {
             parseDiagnostics = [];
             parsingContext = 0;
             identifiers = createMap<string>();
+            privateNames = createMap<string>();
             identifierCount = 0;
             nodeCount = 0;
 
@@ -1430,9 +1432,17 @@ namespace ts {
             return finishNode(node);
         }
 
+        function internPrivateName(text: string): string {
+            let privateName = privateNames.get(text);
+            if (privateName === undefined) {
+                privateNames.set(text, privateName = text);
+            }
+            return privateName;
+        }
+
         function parsePrivateName(): PrivateName {
                 const node = createNode(SyntaxKind.PrivateName) as PrivateName;
-                node.escapedText = escapeLeadingUnderscores(scanner.getTokenText());
+                node.escapedText = escapeLeadingUnderscores(internPrivateName(scanner.getTokenText()));
                 nextToken();
                 return finishNode(node);
         }
