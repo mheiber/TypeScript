d1f73ffe52a6eebc33dec5fa595db90222b80b05
Max Heiber <max.heiber@gmail.com>
2019-07-15 17:25:39 +0100

    rename and simplify privateNameAndAny test case
    make it clearer that all this test is showing is
    that we allow accessing arbitrary private identifiers
    on `any`.
    
    Note: we could have something more sound here by
    checking that there is a private-named field declaration
    in a class scope above the property access.
    
    Discussion:
    https://github.com/microsoft/TypeScript/pull/30829/files#r302760015

---
+++ tests/baselines/reference/privateNameAndAny.js
@@ -0,0 +1,29 @@
+//// [privateNameAndAny.ts]
+// @target es6
+
+class A {
+    #foo = true; 
+    method(thing: any) {
+        thing.#foo; // OK
+        thing.#bar; // OK: we do not (yet) check that #foo is in an ancestor class (but we could)
+    }
+};
+
+
+//// [privateNameAndAny.js]
+// @target es6
+var _classPrivateFieldGet = function (receiver, privateMap) { if (!privateMap.has(receiver)) { throw new TypeError("attempted to get private field on non-instance"); } return privateMap.get(receiver); };
+var _foo;
+"use strict";
+var A = /** @class */ (function () {
+    function A() {
+        _foo.set(this, true);
+    }
+    A.prototype.method = function (thing) {
+        _classPrivateFieldGet(thing, _foo); // OK
+        thing.bar; // OK: we do not (yet) check that #foo is in an ancestor class (but we could)
+    };
+    return A;
+}());
+_foo = new WeakMap();
+;
